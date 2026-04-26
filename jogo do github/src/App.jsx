9db0c65d3427b409b2fb989a4ce7889aec2d4101
andreyribeiro392-coder <andreyribeiro392@gmail.com>
2026-04-25 21:27:20 -0300

    Update App.jsx

--- jogo do github/src/App.jsx
+++ jogo do github/src/App.jsx
@@ -1,1 +1,36 @@
+import { useRef, useState } from "react";
+import { startGame } from "./game";
 
+function App() {
+  const canvasRef = useRef(null);
+  const [jogando, setJogando] = useState(false);
+
+  const iniciar = () => {
+    setJogando(true);
+
+    setTimeout(() => {
+      const canvas = canvasRef.current;
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      startGame(canvas);
+    }, 100);
+  };
+
+  return (
+    <>
+      {!jogando && (
+        <div id="menu">
+          <h1>🔥 BATTLE SHOOTER 🔫</h1>
+          <button onClick={iniciar}>Jogar</button>
+        </div>
+      )}
+
+      <canvas
+        ref={canvasRef}
+        style={{ display: jogando ? "block" : "none" }}
+      />
+    </>
+  );
+}
+
+export default App;
